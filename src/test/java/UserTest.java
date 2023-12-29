0822cfd87c9988a90d964d868ba632e88b209d52
Shrishti Shah <shubhasritadepalli@gmail.com>
2023-12-29 08:29:06 +0530

    sad test cases

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,5 +1,6 @@
 import org.example.UserValidation;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.junit.jupiter.api.Test;
 
@@ -7,29 +8,53 @@ import org.junit.jupiter.api.Test;
 public class UserTest {
 
     @Test
-    public void testValidFirstName() {
+    public void testValidFirstNamehappy() {
         assertTrue(UserValidation.isValidInput("Shubhasri", "^[A-Z][a-zA-Z]{2,}$"));
     }
 
     @Test
-    public void testValidLastName() {
+    public void testinValidFirstNamesad() {
+        assertFalse(UserValidation.isValidInput("subha", "^[A-Z][a-zA-Z]{2,}$"));
+    }
+
+    @Test
+    public void testValidLastNamehappy() {
         assertTrue(UserValidation.isValidInput("Tadepalli", "^[A-Z][a-zA-Z]{2,}$"));
+    }
+
+     @Test
+    public void testinValidLastNamesad() {
+        assertFalse(UserValidation.isValidInput("tadepalli", "^[A-Z][a-zA-Z]{2,}$"));
     }
 
 
     @Test
-    public void testValidEmail() {
+    public void testValidEmailhappy() {
         assertTrue(UserValidation.isValidInput("shubha@example.com", "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"));
     }
 
     @Test
-    public void testValidMobileNumber() {
-        assertTrue(UserValidation.isValidInput("91 9919819801", "^\\d{2} \\d{10}$"));
+    public void testinValidEmailsad() {
+        assertFalse(UserValidation.isValidInput("shubhaexample.com", "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"));
     }
 
     @Test
-    public void testValidPassword() {
+    public void testValidMobileNumberhappy() {
+        assertTrue(UserValidation.isValidInput("91 9919819801", "^\\d{2} \\d{10}$"));
+    }
+    @Test
+    public void testinValidMobileNumbersad() {
+        assertFalse(UserValidation.isValidInput("9919819801", "^\\d{2} \\d{10}$"));
+    }
+
+    @Test
+    public void testValidPasswordhappy() {
         assertTrue(UserValidation.isValidPassword("Shubha@1234"));
+    }
+
+    @Test
+    public void testinValidPasswordsad() {
+        assertFalse(UserValidation.isValidPassword("shubha"));
     }
 
 }
